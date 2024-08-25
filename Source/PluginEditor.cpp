2533bc55e6fd44401b7faf1f28339dfa2f4f7e3c
TordurH <tordurhansen@gmail.com>
2024-08-25 02:25:02 +0100

    c

--- Source/PluginEditor.cpp
+++ Source/PluginEditor.cpp
@@ -35,7 +35,6 @@ void ClipperAudioProcessorEditor::paint (juce::Graphics& g)
     // (Our component is opaque, so we must completely fill the background with a solid colour)
     g.fillAll (getLookAndFeel().findColour (juce::ResizableWindow::backgroundColourId));
 
-   
 }
 
 void ClipperAudioProcessorEditor::resized()
